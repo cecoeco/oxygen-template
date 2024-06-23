edb84598f422c7a6a7e6369f2c8bad6d78b8bcad
webofceco <mapleselijah9@gmail.com>
2024-06-22 20:53:38 -0400

    removed HTTP.jl

--- app.jl
+++ app.jl
@@ -1,52 +1,33 @@
-using Oxygen, HTTP
+using Oxygen: get, html, serve
 
-const ALLOWED_ORIGINS::Vector{Pair{String,String}} = [
-    "Access-Control-Allow-Origin" => "*"
-]
+html_string::String = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Random Number Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+</head>
+<body class="container mt-5">
+    <h1 class="mb-4">Random Number Generator</h1>
+    <button class="btn btn-primary mb-3" onclick="generateRandomNumber()">
+        Generate Random Number
+    </button>
+    <p id="randomNumber" class="lead"></p>
+    <script>
+        function generateRandomNumber() {
+            var randomNumber = Math.floor(Math.random() * 100) + 1;
+            document.getElementById("randomNumber").innerText =
+                "Random Number: " + randomNumber;
+        }
+    </script>
+</body>
+</html>
+"""
 
-const CORS_HEADERS::Vector{Pair{String,String}} = [
-    ALLOWED_ORIGINS...,
-    "Access-Control-Allow-Headers" => "*",
-    "Access-Control-Allow-Methods" => "GET, POST, OPTIONS"
-]
-
-function CorsHandler(handle::Function)
-    return function (req::HTTP.Request)
-        if HTTP.method(req) == "OPTIONS"
-            return HTTP.Response(200, CORS_HEADERS)
-        else
-            r = handle(req)
-            Base.append!(r.headers, ALLOWED_ORIGINS)
-            return r
-        end
-    end
+get("/") do
+    html(html_string)
 end
 
-Oxygen.get("/") do
-    Oxygen.html(
-        """
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Random Number Generator</title>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        </head>
-        <body class="container mt-5">
-            <h1 class="mb-4">Random Number Generator</h1>
-            <button class="btn btn-primary mb-3" onclick="generateRandomNumber()">Generate Random Number</button>
-            <p id="randomNumber" class="lead"></p>
-            <script>
-                function generateRandomNumber() {
-                    var randomNumber = Math.floor(Math.random() * 100) + 1;
-                    document.getElementById("randomNumber").innerText = "Random Number: " + randomNumber;
-                }
-            </script>
-        </body>
-        </html>
-        """
-    )
-end
-
-Oxygen.serve(host="0.0.0.0", port=5050, middleware=[CorsHandler], docs=false, metrics=false)
+serve(host="0.0.0.0", port=5050, docs=false, metrics=false)
